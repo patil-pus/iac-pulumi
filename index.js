3b641e8bb26ccc0a58883454d9324275b8c433df
Pushkar Patil <114626661+patil-pus@users.noreply.github.com>
2023-12-04 19:40:34 -0500

    Update index.js
    Added SSL

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
     const { Script } = require("@pulumi/aws/gamelift");
     const { LoadBalancer } = require("@pulumi/aws/alb");
     const gcp = require("@pulumi/gcp");
-
     const config = new pulumi.Config();
     const gcpProject = config.require("gcpproject");
 
@@ -458,7 +457,6 @@ const testAttach = new aws.iam.RolePolicyAttachment("testAttach", {
         
         
     const autoScalingGroup = new aws.autoscaling.Group("myAutoScalingGroup", {
-
             name:"auto_scaling_group",
             launchTemplate: {
                 id: webAppLaunchTemplate.id,
@@ -554,7 +552,6 @@ const testAttach = new aws.iam.RolePolicyAttachment("testAttach", {
 
         exports.secretkeyaccess=pulumi.secret(serviceAccountKey.privateKey)
         const bucket = new gcp.storage.Bucket("Backet", {
-
         name: "bucket-new-pushka",
         location: "us-east1", 
     });
@@ -580,7 +577,6 @@ const testAttach = new aws.iam.RolePolicyAttachment("testAttach", {
         });
             
     const tableName = "emailsSent";
-
 
     
 const dynamoDbTable = new aws.dynamodb.Table("dynamoDbTable", {
@@ -650,52 +646,6 @@ const dynamoDbTable = new aws.dynamodb.Table("dynamoDbTable", {
                 ".": new pulumi.asset.FileArchive("C:/Users/pushk/OneDrive/Desktop/Serverless/serverless_fork/Serverless.zip"),
             }),
             packageType: "Zip",
-            runtime: "nodejs18.x",
-            role: lambdaRole.arn,
-            handler: "Serverless/index.handler",
-            environment: { 
-                variables: {
-                    GOOGLE_CLIENT_EMAIL:"pushkar.patil1269@gmail.com",
-                    GOOGLE_ACCESS_KEY: serviceAccountKey.privateKey.apply(key => Buffer.from(key, 'base64').toString('ascii')),
-                    BUCKET_NAME: bucket.name,
-                    SNS_TOPIC_ARN: snsTopic.arn, 
-                    MAILGUN_API_KEY: MAILGUN_API_KEY,
-                    Domain_Name: config.require("rootdomain"),
-                    dynamoTableName:dynamoTableName
-                },
-        
-            }
-        });
-
-
-    
-        const dynamoTableName = dynamoDbTable.name;
-        const emailsSentTablePolicy = new aws.iam.Policy("emailsSentTablePolicy", {
-            policy: {
-                "Version": "2012-10-17",
-                "Statement": [
-                    {
-                        "Effect": "Allow",
-                        "Action": [
-                            "dynamodb:GetItem",
-                            "dynamodb:PutItem",
-                            "dynamodb:UpdateItem",
-                            "dynamodb:BatchWriteItem",
-                            "dynamodb:Query",
-                            "dynamodb:Scan",
-                            "dynamodb:DeleteItem"          
-                        ],
-                        "Resource": "*"
-                    }
-          ]
-        },
-          });
-        
-        const submissionLambda = new aws.lambda.Function("submissionLambda", {
-            code: new pulumi.asset.AssetArchive({
-                ".": new pulumi.asset.FileArchive("C:/Users/pushk/OneDrive/Desktop/Serverless/serverless_fork/Serverless.zip"),
-            }),
-            packageType: "Zip",
             timeout: 20,
             memorySize: 256,
             runtime: "nodejs18.x",
@@ -714,7 +664,6 @@ const dynamoDbTable = new aws.dynamodb.Table("dynamoDbTable", {
         
             }
         });
-
 
         const dynamoEmailPolicyAttachment = new aws.iam.RolePolicyAttachment("dynamoEmailPolicyAttachment", {
             role: lambdaRole.name,
@@ -787,6 +736,4 @@ const dynamoDbTable = new aws.dynamodb.Table("dynamoDbTable", {
         }],
     }, { dependsOn: [webAppLoadBalancer] });   
 });
-
     });
-
